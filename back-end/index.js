46affba5619d9b7825adc7a444a597f83cf2796e
max125-c <pravintambe5671@gmail.com>
2025-04-01 08:38:43 +0530

    Backend word properly but front end is remaining

--- back-end/index.js
+++ back-end/index.js
@@ -13,18 +13,24 @@ const genAI = new GoogleGenerativeAI(GOOGLE_API_KEY);
 
 const model = genAI.getGenerativeModel({model:"gemini-1.5-flash"});
 
+app.use(express.json());
+
 app.get('/', async (req, res) => {
+
+    const question = req.body.question;
+
+    console.log(question);
 
     const chat = model.startChat({
         history: [],
     });
-    const result = await chat.sendMessage("i want to learn the full concept of the front end and backend give mi all start to end learning path with and explanation how i done this   ??");
+    const result = await chat.sendMessage(question);
     const response = await result.response;
-    const text = response.text();
+    const text = await response.text();
 
     // send the response returned by the model as API response
 
-    res.send({test: text});
+    res.send({Response: text});
 })
 
 app.listen(port, () => {
